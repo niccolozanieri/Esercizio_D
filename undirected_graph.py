8c84d100ff828deda749449da9f50000ca80d259
Nick <niccolo.zanieri.13@gmail.com>
2021-08-26 17:17:40 +0200

    Added UndirectedGraph class and c'tor

---
+++ undirected_graph.py
@@ -0,0 +1,21 @@
+import numpy as np
+from random import randint
+
+from undirected_graph_vertex import UndirectedGraphVertex
+from undirected_graph_edge import UndirectedGraphEdge
+
+
+class UndirectedGraph:
+    def __init__(self, n=0):
+        self.adjacency_matrix = np.zeros((n, n))
+        self.vertices = []
+        self.edges = []
+
+        for i in range(0, n):
+            self.vertices.append(UndirectedGraphVertex(i + 1))
+
+        for i in range(0, n):
+            for j in range(i + 1, n):
+                if randint(1, 10) <= 6:
+                    self.adjacency_matrix[i, j] = 1
+                    self.adjacency_matrix[j, i] = 1
