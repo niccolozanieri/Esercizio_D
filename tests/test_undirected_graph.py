41d32f123e2e2ae3ca16ecb28e96fa68f1d2f733
Nick <niccolo.zanieri.13@gmail.com>
2021-08-27 10:31:35 +0200

    Modified initialization of UndirectedGraph by adding initialize method

--- tests/test_undirected_graph.py
+++ tests/test_undirected_graph.py
@@ -2,11 +2,20 @@ import unittest
 import io
 import sys
 import random
+import numpy as np
 
 from undirected_graph import UndirectedGraph
+from undirected_graph_edge import UndirectedGraphEdge
 
 
 class MainTest(unittest.TestCase):
+    def test_ctor(self):
+        graph = UndirectedGraph(3)
+        graph.initialize(0)
+        self.assertEqual(graph.find_edge(UndirectedGraphEdge(graph.vertices[0], graph.vertices[1])), True)
+        self.assertEqual(graph.find_edge(UndirectedGraphEdge(graph.vertices[1], graph.vertices[2])), True)
+        self.assertEqual(graph.adjacency_matrix.shape, (3, 3))
+
     def test_to_string(self):
         graph = UndirectedGraph()
         captured_output = io.StringIO()
@@ -15,13 +24,13 @@ class MainTest(unittest.TestCase):
         sys.stdout = sys.__stdout__
         self.assertEqual(captured_output.getvalue(), '')
 
-        random.seed(5)
         graph = UndirectedGraph(3)
+        graph.initialize(0)
         captured_output = io.StringIO()
         sys.stdout = captured_output
         graph.to_string()
         sys.stdout = sys.__stdout__
-        printed_matrix = '0 0 1 \n0 0 1 \n1 1 0 \n'
+        printed_matrix = '0 1 0 \n1 0 1 \n0 1 0 \n'
         self.assertEqual(captured_output.getvalue(), printed_matrix)
 
 
--- undirected_graph.py
+++ undirected_graph.py
@@ -13,17 +13,33 @@ class UndirectedGraph:
         self.edges = []
 
         for i in range(0, n):
-            self.vertices.append(UndirectedGraphVertex(i + 1))
+            self.vertices.append(UndirectedGraphVertex(i))
 
-        for i in range(0, n):
-            for j in range(i + 1, n):
-                if randint(1, 10) <= 6:
-                    self.adjacency_matrix[i, j] = 1
-                    self.adjacency_matrix[j, i] = 1
-                    self.edges.append(UndirectedGraphEdge(self.vertices[i], self.vertices[j]))
+    def initialize(self, rand=1):
+        if rand == 1:
+            for i in range(0, self.n):
+                for j in range(i + 1, self.n):
+                    if randint(1, 10) <= 6:
+                        self.adjacency_matrix[i, j] = 1
+                        self.adjacency_matrix[j, i] = 1
+                        self.edges.append(UndirectedGraphEdge(self.vertices[i], self.vertices[j]))
+        else:
+            for i in range(0, self.n):
+                for j in range(i + 1, self.n):
+                    if (i + j) % 2 == 1:
+                        self.adjacency_matrix[i, j] = 1
+                        self.adjacency_matrix[j, i] = 1
+                        self.edges.append(UndirectedGraphEdge(self.vertices[i], self.vertices[j]))
 
     def to_string(self):
         for i in range(0, self.n):
             for j in range(0, self.n):
                 print(self.adjacency_matrix[i, j], end=' ')
             print('\n', end='')
+
+    def find_edge(self, edge):
+        for item in self.edges:
+            if item.is_equal(edge):
+                return True
+
+        return False
